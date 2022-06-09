ef2c9c7c2e669f2f4b7a4ba5735c4fbb99512c90
jeanvegadev <jcvega.dev@gmail.com>
2022-06-08 23:45:29 -0500

    no jquery

--- js/function.js
+++ js/function.js
@@ -1,28 +1,3 @@
-
-var stickyOffset = 5;
-
-$(window).scroll(function(){
-      var sticky = $('.sticky'),
-          scroll = $(window).scrollTop();
-      if (scroll >= stickyOffset) sticky.removeClass('py-3');
-      else sticky.addClass('py-3');
-      });
-
-$(document).ready(function() {
-        $('.counter').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-            }, {
-      duration: 2000,
-      easing: 'swing',
-      step: function (now) {
-        $(this).text(Math.ceil(now));
-        }
-      });
-      });
-
-      });
-
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -42,3 +17,32 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Animación de los números
+document.addEventListener("DOMContentLoaded", function() {
+  elements = document.querySelectorAll(".counter");
+  const speed = 200;
+  elements.forEach( counter => {
+    const animate = () => {
+      const value = counter.getAttribute('akhi');
+      const data = +counter.innerText;
+      const time = value / speed;
+      if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+      }else{
+          counter.innerText = value;
+      }
+    }
+    animate();
+    });
+  });
+
+// Apega la barra de inicio hacia arriba cuando la página se mueve hacia abajo
+window.addEventListener('scroll', function(e){
+    let stickyOffset = 5;
+    let sticky = document.querySelector(".sticky"),
+    scroll = window.scrollY;
+    if (scroll >= stickyOffset) sticky.classList.remove('py-3');
+    else sticky.classList.add('py-3');
+  });
